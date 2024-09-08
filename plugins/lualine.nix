55b6198395a3f9cb3fd47c48fe79a0672e240c30
Alkim Ake Gozen <alkimake@pm.me>
2024-09-08 11:20:07 +0900

    lualine initial

---
+++ plugins/lualine.nix
@@ -0,0 +1,9 @@
+
+# homepage: https://github.com/nvim-lualine/lualine.nvim
+# nixvim doc: https://nix-community.github.io/nixvim/plugins/lualine/index.html
+_: {
+  opts = {
+    enable = true;
+
+  };
+}
