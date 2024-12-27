e3da48aafde30bb4e271bf02595d26777db9a350
Alkim Ake Gozen <alkim@lviscorp.com>
2024-12-27 15:20:28 +0900

    make lualine catppuccin theme

--- plugins/lualine.nix
+++ plugins/lualine.nix
@@ -1,9 +1,14 @@
-
 # homepage: https://github.com/nvim-lualine/lualine.nvim
 # nixvim doc: https://nix-community.github.io/nixvim/plugins/lualine/index.html
 _: {
   opts = {
     enable = true;
-
+    settings = {
+      options = {
+        globalstatus = true;
+        icons_enabled = true;
+        theme = "catppuccin";
+      };
+    };
   };
 }
